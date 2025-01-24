7961c9b415810e58e07a04d32f6e0b2672be4c02
Lauren245 <102630328+Lauren245@users.noreply.github.com>
2025-01-23 16:53:52 -0800

    added saveCandidate function to CandidateSearch.

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -50,33 +50,48 @@ const CandidateSearch = () => {
       }
   }, [users]); //this effect runs when the user state is updated
 
+  const saveCandidate = () => {
+        console.log('running saveCandidate');
+        let parsedCandidateToSave: Candidate[] = [];
+        const storedCandidates = localStorage.getItem('savedCandidates');
+        if(typeof storedCandidates === 'string'){
+            parsedCandidateToSave = JSON.parse(storedCandidates);
+        }
+        parsedCandidateToSave.push(gitHubUser);
+        localStorage.setItem('savedCandidates', JSON.stringify(parsedCandidateToSave));
+  }
+
   return (
       <>
           <h1>CandidateSearch</h1>
           {gitHubUser && (
-              <div className="card">
-                  <div className="card-header">
-                      <img src={gitHubUser.avatar_url || ''} alt="The GitHub user avatar" className="avatar" />
-                      <h2>{gitHubUser.login}</h2>
-                  </div>
+            <>
+                <div className="card">
+                    <div className="card-header">
+                        <img src={gitHubUser.avatar_url || ''} alt="The GitHub user avatar" className="avatar" />
+                        <h2>{gitHubUser.login}</h2>
+                    </div>
 
-                  <div className="card-body">
-                      <p><strong>Developer Name:</strong> {gitHubUser.name || 'no name given'}</p>
-                      <p><strong>Location:</strong> {gitHubUser.location || 'no location'}</p>
-                      <p><strong>Email:</strong> {gitHubUser.email || 'no email'}</p>
-                      <p><strong>Company:</strong> {gitHubUser.company || 'no company listed'}</p>
-                      <p><strong>Number of public repositories:</strong> {gitHubUser.public_repos}</p>
-                      <p><strong>Bio:</strong> {gitHubUser.bio || 'no bio'}</p>
-                  </div>
+                    <div className="card-body">
+                        <p><strong>Developer Name:</strong> {gitHubUser.name || 'no name given'}</p>
+                        <p><strong>Location:</strong> {gitHubUser.location || 'no location'}</p>
+                        <p><strong>Email:</strong> {gitHubUser.email || 'no email'}</p>
+                        <p><strong>Company:</strong> {gitHubUser.company || 'no company listed'}</p>
+                        <p><strong>Number of public repositories:</strong> {gitHubUser.public_repos}</p>
+                        <p><strong>Bio:</strong> {gitHubUser.bio || 'no bio'}</p>
+                    </div>
 
-                  <div className="card-footer">
-                      <a href={gitHubUser.html_url || ''} target="_blank" rel="noopener noreferrer" className="profile-link">
-                        View GitHub Profile
-                      </a>
-                  </div>
+                    <div className="card-footer">
+                        <a href={gitHubUser.html_url || ''} target="_blank" rel="noopener noreferrer" className="profile-link">
+                          View GitHub Profile
+                        </a>
+                    </div>
 
-              </div>
+                </div>
+                <button type='button' onClick={saveCandidate}>Add</button>
+            </>
           )}
+          
       </>
   );
 };
