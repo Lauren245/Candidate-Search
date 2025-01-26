ddfed71b8deb8b400f904d14cfc2afd19d51f364
Lauren245 <102630328+Lauren245@users.noreply.github.com>
2025-01-25 16:04:06 -0800

    added localStorage getItem function.

--- src/pages/SavedCandidates.tsx
+++ src/pages/SavedCandidates.tsx
@@ -1,9 +1,17 @@
-
+//import Candidate from '../interfaces/Candidate.interface';
 
 const SavedCandidates = () => {
+
+  const candidate: string[] = JSON.parse(localStorage.getItem('savedCandidates') || '[]')
   return (
     <>
       <h1>Potential Candidates</h1>
+      {/* <table>
+        <tr>
+            <th>{}</th>
+        </tr>
+      </table> */}
+      <p>{candidate}</p>
     </>
   );
 };
