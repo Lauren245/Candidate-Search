4ef41962478b70d07b5253e4b8d1dda5fd2dcc15
Lauren245 <102630328+Lauren245@users.noreply.github.com>
2025-01-25 16:56:40 -0800

    got user data for potential candidates table working. I will need to go back and add a reject button later.

--- src/pages/SavedCandidates.tsx
+++ src/pages/SavedCandidates.tsx
@@ -1,17 +1,39 @@
-//import Candidate from '../interfaces/Candidate.interface';
+import Candidate from '../interfaces/Candidate.interface';
 
 const SavedCandidates = () => {
 
-  const candidate: string[] = JSON.parse(localStorage.getItem('savedCandidates') || '[]')
+  const candidateArr: Candidate[] = JSON.parse(localStorage.getItem('savedCandidates') || '[]')
   return (
     <>
       <h1>Potential Candidates</h1>
-      {/* <table>
-        <tr>
-            <th>{}</th>
-        </tr>
-      </table> */}
-      <p>{candidate}</p>
+      <table>
+        <thead>
+          <tr>
+              {/* I opted to harcode these instead of getting them from the array because the key names are not very nice looking. */}
+              <th>Image</th>
+              <th>Name</th>
+              <th>Location</th>
+              <th>Email</th>
+              <th>Company</th>
+              <th>Bio</th>
+              <th>Reject</th>
+            </tr>
+        </thead>
+        <tbody>
+          {candidateArr.map((candidate) => (
+            <tr key={candidate.login}>
+              <td><img src={candidate.avatar_url || ''} alt="The GitHub user avatar" className="avatar-scaled" /></td>
+              <td>{candidate.name || 'no name given'}<br />{candidate.login}</td>
+              <td>{candidate.location || 'no location'}</td>
+              <td>{candidate.email || 'no email'}</td>
+              <td>{candidate.company || 'no company listed'}</td>
+              <td>{candidate.bio || 'no bio'}</td>
+              <td>Button goes here</td>
+            </tr>
+            ))}
+        </tbody>
+      </table>
+
     </>
   );
 };
