0aac4f762e67ec55e345c8e39413675bd4168f11
Lauren245 <102630328+Lauren245@users.noreply.github.com>
2025-01-28 11:25:55 -0800

    implemented navbar logic. Still needs styling.

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,30 @@
+import { Link, useLocation } from 'react-router-dom';
+
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
+  const currentPage = useLocation().pathname;
+
   return (
-    <div>Nav</div>
+    <div className='nav'>
+      <ul className='navbar-nav'>
+        <li className='nav-item'>
+          <Link
+            to='/'
+            className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
+          >
+            Search Candidates
+          </Link>
+        </li>
+        <li className='nav-item'>
+          <Link
+            to= '/SavedCandidates'
+            className={currentPage === '/SavedCandidates' ? 'nav-link active' : 'nav-link'}
+          >
+            View Saved Candidates
+          </Link>
+        </li>
+      </ul>
+    </div>
   )
 };
 
